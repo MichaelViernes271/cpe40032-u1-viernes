dc31cb6fd0da8240c9375e3c6ac1e2d41ad98daa
Michael <michaelviernes.271@outlook.com>
2022-11-20 09:57:45 +0800

    unit 1 complete

---
+++ Assets/Course Library/Scripts/PropellerSpinX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpinX : MonoBehaviour
+{
+    private Vector3 Spin = new Vector3(0, 0, 1000);
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Rotate( Spin * Time.deltaTime); 
+
+    }
+}
